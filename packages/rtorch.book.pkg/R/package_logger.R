06ca5e8c81a60b8a2c755c47a61018c211e2907d
fonzie zbook <alfonso.reyes@oilgainsanalytics.com>
2019-08-10 11:50:33 -0500

    add package for book

---
+++ packages/rtorch.book.pkg/R/package_logger.R
@@ -0,0 +1,31 @@
+#----------------------------------------------------------------------------
+# rtorch.book.pkg
+#
+# Package logger setup
+#----------------------------------------------------------------------------
+
+.logger_name <- "rtorch.book.pkg"
+.pkg_logger <- logging::getLogger(.logger_name)
+.pkg_logger$setLevel("FINEST")
+
+pkg_loginfo <- function(msg, ...) tryCatch(logging::loginfo(msg, ..., logger = .pkg_logger),
+                                           error = function(e) warning(e))
+pkg_logdebug <- function(msg, ...) tryCatch(logging::logdebug(msg, ..., logger = .pkg_logger),
+                                            error = function(e) warning(e))
+pkg_logerror <- function(msg, ...) tryCatch(logging::logerror(msg, ..., logger = .pkg_logger),
+                                            error = function(e) warning(e))
+pkg_logwarn <- function(msg, ...) tryCatch(logging::logwarn(msg, ..., logger = .pkg_logger),
+                                           error = function(e) warning(e))
+pkg_logfinest <- function(msg, ...) tryCatch(logging::logfinest(msg, ..., logger = .pkg_logger),
+                                             error = function(e) warning(e))
+
+#'
+#' Retrieves rtorch.book.pkg logger.
+#' 
+#' @return logger object
+#' 
+#' @export
+#' 
+rtorch.book.pkg_getLogger <- function() {
+  .pkg_logger
+}
